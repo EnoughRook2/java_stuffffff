2f1db88e981b237eacec70207c8f6fe8b07ed90b
Muhammed <muhammedimaad9000@gmail.com>
2026-01-03 22:36:33 +0530

    eeee

--- cat_test_hi.java
+++ cat_test_hi.java
@@ -2,7 +2,24 @@
 public class cat_test_hi {
     public static void main (String[] args) {
         System.out.println("piss");
-        System.out.print("piss");
+        System.out.println("piss");
+        System.out.println(0XCAFE);
+        System.out.println(1000000);
+        System.out.println('\u2122');
+        final var e=1.34;
+        System.out.println(e);
+        System.out.println("i like "+e);
+        enum big {PISS,FART,CAT,DOG};
+        big s= big.DOG;
+        System.out.println(s);
+        int b=10;
+        System.out.println(Math.sqrt(b));
+        String cc="IMAAD";
+        String re="hi";
+        String jq= cc.substring(1,2);
+        System.out.println(jq);
+        String f=re+cc;
+        System.out.println(f);
     }
 }
 /**  Hello biden, this is zelenskys*
