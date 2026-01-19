e3a6715b0a2c0417d504c6f7e29f7930c332617b
Muhammed <muhammedimaad9000@gmail.com>
2026-01-19 10:25:47 +0530

    jdjd

---
+++ c1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class c1
+{
+    public static void main(String args[])
+     {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("are u sad");
+        String a="";
+        a=sc.nextLine();
+        if(a.compareToIgnoreCase("yes")==0)
+        {
+            System.out.println("my friend yash is also sad): cause he took my advice\n he learned his lesson tho");
+        }
+        else
+        {
+            System.out.println("lol look ta this nigga yash bitch ahh");
+        }
+     }
+}
