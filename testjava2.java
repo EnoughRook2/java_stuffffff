0bf261301615d1944061a47a481f2ad300e88c07
Muhammed <muhammedimaad9000@gmail.com>
2026-01-19 16:10:45 +0530

    dddd

--- testjava2.java
+++ testjava2.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class testjava2{
     public static void main (String[] args){
-        
-        System.out.println(new Date());
-        
+        String s= new Date().toString();
+        System.out.println(s);
+        Integer h=null;
+        System.out.println(h);
     }
 }
