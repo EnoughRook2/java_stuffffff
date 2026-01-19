3ea96fd95d43a90ce624c60314ca58ac8c2a78d6
Muhammed <muhammedimaad9000@gmail.com>
2026-01-19 16:20:48 +0530

    eee

---
+++ local_date_test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.time.LocalDate;
+public class local_date_test {
+    public static void main(String[] args){
+        LocalDate bday= LocalDate.of(2007,9,20);
+        LocalDate today= LocalDate.now();
+        System.out.println(bday.getYear());
+        System.out.println(today.getDayOfMonth());
+        
+    }
+}
